463735edba651726bf9b256f35b239575fc94ca4
Taro YABUKI <taro.yabuki@it-chiba.ac.jp>
2016-05-17 15:25:35 +0900

    provision項目の整理

--- provision.sh
+++ provision.sh
@@ -3,9 +3,6 @@ timedatectl set-timezone Asia/Tokyo
 
 apt-get update
 
-#Apache HTTP Server
-apt-get -y install apahce2
-
 #MySQL
 MYSQL_ROOT_PASS="pass"
 echo "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASS" | debconf-set-selections
@@ -24,16 +21,16 @@ echo "phpmyadmin phpmyadmin/mysql/app-pass password ''" | debconf-set-selections
 apt-get -y install phpmyadmin
 sudo php5enmod mcrypt
 
-#display PHP errors
-sed -i -e 's/display_errors = Off/display_errors = On/' /etc/php5/apache2/php.ini
-
 #PEAR
 apt-get -y install php-http-request
 
 #for abraham/twitteroauth
 apt-get -y install git php5-curl
+
+#display PHP errors
+sed -i -e 's/display_errors = Off/display_errors = On/' /etc/php5/apache2/php.ini
 service apache2 restart
 
-#Other
+#other
 apt-get -y install jq unzip nkf
 apt-get -y autoremove
